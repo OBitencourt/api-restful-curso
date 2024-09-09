33e5311ddb0662e64db8c594796cbcbcdf43bb4d
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-09-09 21:20:03 +0000

    initial project estructure

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+// const path = require('path')
+
+const app = express()
+
+//conexão com banco de dados
+// db.connect()
+
+const routes = require('./routes/routes')
+
+// habilita server para receber dados via post (form)
+
+app.use(express.urlencoded({express: true}))
+
+
+// habilitando rotas
+
+
+app.use('/api', routes)
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
